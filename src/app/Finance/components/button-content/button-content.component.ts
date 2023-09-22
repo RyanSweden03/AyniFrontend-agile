33821e3e9b2172520a52dec2090713debd1b83bd
JorgeGonzales15 <u202118468@upc.edu.pe>
2023-09-22 15:29:31 -0500

    feat: added cost and bills list and buttons

---
+++ src/app/Finance/components/button-content/button-content.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-button-content',
+  templateUrl: './button-content.component.html',
+  styleUrls: ['./button-content.component.css'],
+
+
+})
+export class ButtonContentComponent {
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,33 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
+import { ListContentComponent } from './Finance/components/list-content/list-content.component';
+import { List2ContentComponent } from './Finance/components/list2-content/list2-content.component';
+import {MatIconModule} from '@angular/material/icon';
+import {MatDividerModule} from '@angular/material/divider';
+import {MatButtonModule} from '@angular/material/button';
+import { ButtonContentComponent } from './Finance/components/button-content/button-content.component';
+import { Button2ContentComponent } from './Finance/components/button2-content/button2-content.component';
+import { FinanceMainContentComponent } from './Finance/components/finance-main-content/finance-main-content.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListContentComponent,
+    List2ContentComponent,
+    ButtonContentComponent,
+    Button2ContentComponent,
+    FinanceMainContentComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatIconModule,
+    MatDividerModule,
+    MatButtonModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
