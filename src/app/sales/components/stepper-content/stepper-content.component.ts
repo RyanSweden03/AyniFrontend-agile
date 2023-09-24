c2594d7620a5cf962ac38b53a24c659a2db4e299
Paolo Espejo <u20211b786@upc.edu.pe>
2023-09-24 18:55:31 -0500

    feat: Added sales stepper component

---
+++ src/app/sales/components/stepper-content/stepper-content.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'sales-stepper-content',
+  templateUrl: './stepper-content.component.html',
+  styleUrls: ['./stepper-content.component.css']
+})
+export class SalesStepperContentComponent {
+
+  constructor() {}
+
+}
