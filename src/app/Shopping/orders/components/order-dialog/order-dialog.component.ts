e80c0c3368a7c52b616093ea52d01087e48b789b
TMedalith <u20211f955@upc.edu.pe>
2023-10-13 01:48:31 -0500

    feat(orders): Added order dialog component

---
+++ src/app/Shopping/orders/components/order-dialog/order-dialog.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import {MatDialog, MatDialogModule} from "@angular/material/dialog";
+import {MatButtonModule} from "@angular/material/button";
+
+@Component({
+  selector: 'app-order-dialog',
+  templateUrl: './order-dialog.component.html',
+  styleUrls: ['./order-dialog.component.css']
+})
+export class OrderDialogComponent {
+}
